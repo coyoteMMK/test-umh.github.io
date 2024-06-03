ab5acf20e6f139fee2411a8c5d29fceef267eaf6
coyoteMMK <136488604+coyoteMMK@users.noreply.github.com>
2024-06-03 11:00:15 +0200

    Update data.js

---
+++ sirh2/data.js
@@ -0,0 +1,26 @@
+const data = {
+  item1: {
+    id: "1",
+    title: "Un sistema es un conjunto cosas que ...",
+    first: "Están relacionadas ordenadamente para procurar un determinado objetivo",
+    second:
+      "Buscan alcanzar un mismo objetivo aunque no sigan un orden concreto",
+    third:
+      "Se conectan entre sí, aunque pueden estar pasivas sin hacer una en particular",
+    fourth:
+      "Se relacionan por ser de la misma naturaleza o estar compuestos de los mismos materiales principales",
+    answer: 1,
+  },
+  item2: {
+    id: "2",
+    title: "Un sistema es un conjunto de elementos que ...",
+    first:
+      "Están interconectados según un orden y hacen alguna función de interés",
+    second:
+      "Ocupan un mismo lugar estén o no interrelacionados",
+    third: "Están conectados pero pueden estar inertes sin hacer una función concreta",
+    fourth:
+      "Se caracterizan por una cualidad común (Mismo color, mismo peso, etc.)",
+    answer: 1,
+  },
+};
