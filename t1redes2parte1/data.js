f08193c4c40bf1512e15bea9b1e8f23c96828424
coyoteMMK <136488604+coyoteMMK@users.noreply.github.com>
2024-11-07 18:21:55 +0100

    Create data.js

---
+++ t1redes2parte1/data.js
@@ -0,0 +1,436 @@
+const data = {
+  item1: {
+    id: "1",
+    title: "Indica el número de direcciones de host posibles de la red FullClass a la que pertenece la dirección 175.16.35.312",
+    first: "65534",
+    second:
+      "254",
+    third:
+      "16777214",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 1,
+  },
+  item2: {
+    id: "2",
+    title: "Una dirección válida de loopback es",
+    first:
+      "128.0.0.1",
+    second:
+      "0.0.0.127",
+    third: "127.1.0.0",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 3,
+  },
+  item3: {
+    id: "3",
+    title: "Las direcciones de red IP se dividen en las clases",
+    first:
+      "Únicamente Clase A, B y C",
+    second:
+      "Únicamente Clase A, B, C y D",
+    third:
+      "Únicamente Clase A,B,C, D y E",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 3,
+  },
+  item4: {
+    id: "4",
+    title: "La máscara utilizada para configurar la ruta por defecto es:",
+    first:
+      "0.0.0.0",
+    second:
+      "255.0.0.0",
+    third:
+      "255.255.255.0",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 1,
+  },
+  item5: {
+    id: "5",
+    title: "El protocolo que permite administrar y asignar direcciones IP de forma dinámica desde un servidor a un conjunto de estaciones se denomina:",
+    first:
+      "IP dinámicas Server",
+    second:
+      "DNS",
+    third:
+      "DHCP",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 3,
+  },
+  item6: {
+    id: "6",
+    title: "EL protocolo de transporte orientado a la conexión es:",
+    first:
+      "ICMP",
+    second:
+      "TCP",
+    third:
+      "UDP",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 2,
+  },
+  item7: {
+    id: "7",
+    title: "El protocolo que permite obtener la dirección MAC a partir de la IP mediante encuesta se denomina",
+    first:
+      "RARP",
+    second:
+      "DNS",
+    third:
+      "DHCP",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 4,
+  },
+  item8: {
+    id: "8",
+    title: "El protocolo que permite obtener la dirección MAC a partir de la IP mediante encuesta se denomina",
+    first:
+      "ARP",
+    second:
+      "DNS",
+    third:
+      "RARP",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 1,
+  },
+  item9: {
+    id: "9",
+    title: "El único campo de la cabecera de IPv4 que modifica el Router en el caso que no realice fragmetación es:",
+    first:
+      "TTL",
+    second:
+      "Offset",
+    third:
+      "More Fragments",
+    fourth: "Ninguna de las anteriores",
+    answer: 1,
+  },
+  item10: {
+    id: "10",
+    title: "Indica el número de direcciones de host posibles de la red FullClass a la que pertenece la dirección 15.16.35.312",
+    first:
+      "65534",
+    second:
+      "254",
+    third:
+      "16777214",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 3,
+  },
+  item11: {
+    id: "11",
+    title: "Señalar la afirmación errónea del protocolo ICMP",
+    first:
+      "Informa sólo al origen de la no entrega de un datagrama",
+    second:
+      "Corrige el problema",
+    third:
+      "Viaja en el campo de datos de un datagrama",
+    fourth:
+      "No se considera un protocolo de nivel superior a IP",
+    answer: 2,
+  },
+  item12: {
+    id: "12",
+    title: "Suponiendo una red recien inicializada, donde el equipo A con IP 193.147.136.25/23 realiza un ping al equipo B con IP 193.147.137.26/23 y ambos configurados con el Gateway 193.147.136.1/23. Indica el número de saltos del datagrama hasta alcanzar el destino.",
+    first:
+      "0",
+    second:
+      "1",
+    third: "2",
+    fourth:
+      "Ninguna de las anteriores",
+    answer: 2,
+  },
+  item13: {
+    id: "13",
+    title:
+      "La dirección 129.135.236.0/24 es una dirección de:",
+    first: "Red",
+    second: "Broadcast",
+    third: "Host",
+    fourth: "Ninguna de las anteriores",
+    answer: 1,
+  },
+  item14: {
+    id: "14",
+    title: "La PDU del nivel de red recibe el nombre de:",
+    first:
+      "Datos",
+    second: "Segmento",
+    third: "Datagrama",
+    fourth:
+      "Trama",
+    answer: 3,
+  },
+  item15: {
+    id: "15",
+    title: "Respecto a las características de IPv6, indica la afirmación errónea:",
+    first: "Utiliza un sistema de encaminamiento hoizontal",
+    second: "Permite un formato más flexible de cabecera",
+    third: "Establece un control de ámbitos",
+    fourth: "La cabecera es mas grnade en tamaño que IPv4",
+    answer: 1,
+  },
+  item16: {
+    id: "16",
+    title: "Las direcciones de IPv6 que se utilizan dentro de un mismo enlace o red local para gestion de la propia red se denominan",
+    first: "Globales",
+    second: "Local de Enlace",
+    third: "Local de Sitio",
+    fourth: "De bucle interno",
+    answer: 2,
+  },
+  item17: {
+    id: "17",
+    title: "Indica cual de los siguientes no es un ámbito válido para direcciones de IPv6",
+    first: "Unicast",
+    second: "Multicast",
+    third: "AnyCast",
+    fourth: "BroadCast",
+    answer: 4,
+  },
+  item18: {
+    id: "18",
+    title: "Indica cual de los siguientes campos ha sido elminado de la cabecera fija",
+    first: "Ver",
+    second: "TTL",
+    third: "Offset",
+    fourth: "IP origen",
+    answer: 3,
+  },
+  item19: {
+    id: "19",
+    title: "Indica cual de las siguientes no ha sido una motivación para el desarrollo de IPv4.",
+    first: "Asignación de direcciones no coordinada de IPv4",
+    second: "Escasez de direcciones",
+    third: "Tablas de encaminamiento cada vez más grandes en IPv4",
+    fourth: "Todas son razones que han motivado la aparición de IPv6",
+    answer: 4,
+  },
+  item20: {
+    id: "20",
+    title: "En una red de Ethernet con MTU=910, indica el tamaño máximo de datos de IPv6",
+    first: "870",
+    second: "856",
+    third: "890",
+    fourth: "Ninguna de las anteriores",
+    answer: 2,
+  },
+  item21: {
+    id: "21",
+    title: "En una red de Ethernet con MTU=910, indica el tamaño máximo de datos de IPv4",
+    first: "910",
+    second: "890",
+    third: "888",
+    fourth: "Ninguna de las anteriores",
+    answer: 3,
+  },
+  item22: {
+    id: "22",
+    title: "Una dirección válida de loopback es",
+    first: "128.0.0.1",
+    second: "0.0.0.127",
+    third: "127.1.0.0",
+    fourth: "Ninguna de las anteriores",
+    answer: 3,
+  },
+  item23: {
+    id: "23",
+    title: "Señalar la afirmación errónea del protocolo ICMP",
+    first: "Informa sólo al origen de la no entrega de un datagrama",
+    second: "No corrige el problema",
+    third: "Viaja en el campo de datos de un datagrama",
+    fourth: "Se considera un protocolo de nivel superior a IP",
+    answer: 4,
+  },
+  item24: {
+    id: "24",
+    title: "El único campo de la cabecera de IPv4 que modifica el Router en el caso que no realice fragmetación es:",
+    first: "IHL",
+    second: "Offset",
+    third: "Long. Total",
+    fourth: "Ninguna de las anteriores",
+    answer: 4,
+  },
+  item25: {
+    id: "25",
+    title: "Respecto a IPv6 indica la afirmación errónea",
+    first: "El orden de las cabeceras de extensión es fijo",
+    second: "El número de campos en la cabecera básica es mayor",
+    third: "Se pueden realizar abreviaturas en la dirección de 128 bits",
+    fourth: "La cabecera de extensión de encriptación no aparece en IPv4",
+    answer: 2,
+  },
+  item26: {
+    id: "26",
+    title: "El ámbito relacionado con intranets y redes privadas es",
+    first: "Global",
+    second: "De enlace",
+    third: "De sitio",
+    fourth: "Ninguna de las anteriores",
+    answer: 3,
+  },
+  item27: {
+    id: "27",
+    title: "Indica cual de los siguientes campos ha sido eliminado de la cabecera fija",
+    first: "Ver.",
+    second: "TTL",
+    third: "DF",
+    fourth: "IP origen",
+    answer: 3,
+  },
+  item28: {
+    id: "28",
+    title: "El tiempo en el que una estación no puede garantizar que no ha sufrido colisión en su transmisión se denomina:",
+    first: "Intervalo de contención ",
+    second: "Intervalo de contienda",
+    third: "Intervalo de duda ",
+    fourth: "Ninguna de las anteriores",
+    answer: 2,
+  },
+  item29: {
+    id: "29",
+    title: "El numero de direcciones de red de clase B es:",
+    first: "126",
+    second: "16384",
+    third: "65536",
+    fourth: "Ninguna de las anteriores",
+    answer: 2,
+  },
+  item30: {
+    id: "30",
+    title: "El protocolo de enrutamiento más popular que permite el tipo classless basado en vector de distancias es:",
+    first: "RIPv1",
+    second: "RIPv2 ",
+    third: "OSPF",
+    fourth: "Ninguna de las anteriores",
+    answer: 2,
+  },
+  item31: {
+    id: "31",
+    title: "Indica cual de los siguientes protocolos LAN es libre de colisiones",
+    first: "10BASE2 ",
+    second: "10BASET",
+    third: "10BASE5",
+    fourth: "Todos son protocolos con colisione",
+    answer: 4,
+  },
+  item32: {
+    id: "32",
+    title: "Cual es una direccion valida de lookback es",
+    first: "128.0.0.1",
+    second: "127.1.0.0",
+    third: "0.0.0.127",
+    fourth: "Ninguna de las anteriores",
+    answer: 2,
+  },
+  item33: {
+    id: "33",
+    title: "La longitud máxima de 10Gigabit Ethernet con UTP de categoria 5 es:",
+    first: "100m",
+    second: "20m",
+    third: "15m",
+    fourth: "Ninguna de las anteriores ",
+    answer: 4,
+  },
+  item34: {
+    id: "34",
+    title: "Respecto a Gigabit Ethernet, indica la afirmación errónea",
+    first: "Fue aprobada por el IEEE en 1998 bajo el nombre 802.3z. ",
+    second: "Aumenta diez veces el rendimiento de Fast Ethernet.",
+    third: "Usa el mismo esquema de direccionamiento de 48 bits que ya está en uso",
+    fourth: "Modifica el formato de trama, aumentando el tamaño máximo",
+    answer: 4,
+  },
+  item35: {
+    id: "35",
+    title: "La longitud máxima del segmento en FastEthernet con par trenzado es:",
+    first: "100m",
+    second: "200m",
+    third: "1000m",
+    fourth: "2000m",
+    answer: 1,
+  },
+  item36: {
+    id: "36",
+    title: "En ethernet clásica, para la detección de colisiones, indica la afirmación errónea:",
+    first: "Si aumentamos ancho de banda, tendremos que aumentar la longitud de trama o aumentar la longitud del medio de transmisión",
+    second: "Si aumentamos ancho de banda, tendríamos que aumentar la longitud de trama o reducir la longitud medio de transmisión",
+    third: "Si aumentamos el ancho de banda, tendríamos que reducir la longitud de la trama y/o reducir la longitud del medio de transmisión",
+    fourth: "Todas son erróneas (menos ésta)",
+    answer: 1,
+  },
+  item37: {
+    id: "37",
+    title: "El límite superior de 1500bytes de longitud de datos en la trama de Ethernet clásica vino definido por:",
+    first: "Longitud máxima del canal",
+    second: "Tiempo de propagación de la trama",
+    third: "Tamaño de la memoria en el interfaz de red",
+    fourth: "Ninguna de las anteriores",
+    answer: 3,
+  },
+  item38: {
+    id: "38",
+    title: "En Ethernet Clásico, el cableado que difiere en cuanto a topología física del resto es:",
+    first: "10BASE5",
+    second: "10BASE-T",
+    third: "100BASE-T",
+    fourth: "10BASE2",
+    answer: 2,
+  },
+  item39: {
+    id: "39",
+    title: "El protocolo Ethernet clásico con par trenzado se conoce también como:",
+    first: "10BASE5",
+    second: "100BASE-T",
+    third: "Ninguna de las anteriores",
+    fourth: "10BASE-T",
+    answer: 4,
+  },
+  item40: {
+    id: "40",
+    title: "Un protocolo libre de colisiones que utiliza la misma topología y nivel físico que FastEthernet es:",
+    first: "Token Bus",
+    second: "Token Ring",
+    third: "FDDI",
+    fourth: "Ninguna de las anteriores",
+    answer: 1,
+  },
+  item41: {
+    id: "41",
+    title: "En los protocolos CSMA/CD se define un estado adicional frente a los protocolos ALOHA, indica cual es:",
+    first: "Espera",
+    second: "Transmisión",
+    third: "Recepción",
+    fourth: "Ninguna de las anteriores",
+    answer: 4,
+  },
+  item42: {
+    id: "42",
+    title: "Indica cual de los siguientes protocolos tiene un comportamiento mejor en términos de throughput ante el aumento de tráfico:",
+    first: "ALOHA",
+    second: "ALOHA ranurado",
+    third: "1-persistente CSMA",
+    fourth: "0.5-persistente CSMA",
+    answer: 4,
+  },
+  item43: {
+    id: "43",
+    title: "En enlaces de difusión con medio compartido, indica cual de los siguientes NO es un supuesto clave para la utilización del canal:",
+    first: "Tráfico dependiente entre las estaciones",
+    second: "Supuesto de canal único",
+    third: "Colisiones observables",
+    fourth: "Tiempo continuo o ranurado",
+    answer: 1,
+  },
+};
